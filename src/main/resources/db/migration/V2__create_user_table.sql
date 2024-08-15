473731d7b802375a49a28f84f00fbe5af594bd86
Sam Dagnall <sam.dagnall@kainos.com>
2024-08-15 09:12:22 +0100

    Removed Migrations from initial forked repo

--- src/main/resources/db/migration/V2__create_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user (
-    id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    username varchar(20) NOT NULL,
-    password varchar(20) NOT NULL,
-    roleID int,
-    FOREIGN KEY (roleID) REFERENCES role(id)
-);
--- src/main/resources/db/migration/V3__create_users.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO role (title) VALUES ('Admin');
-INSERT INTO role (title) VALUES ('Employee');
-
-INSERT INTO user (username, password, roleID) VALUES
-    ('sdagn1', 'passwd', 1);
-
-INSERT INTO user (username, password, roleID) VALUES
-    ('schmam', 'schmagnall', 2);
